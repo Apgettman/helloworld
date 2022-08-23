b08d8e3324b8fdc330e5da50afa5cdb71fa37af6
679 <apgettman@gmail.com>
2022-08-23 21:56:20 +0300

    ДЗ-1

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        // Задание 1
+        var dog = 8;
+        var cat = 3.6;
+        var paper = 763789L;
+
+        // Задание 2
+        dog = dog + 4;
+        cat = cat + 4;
+        paper = paper + 4;
+
+        // Задание 3
+        var dogWeight = dog - 3.5;
+        var catWeight = cat - 1.6;
+        var paperWeight = paper - 7639;
+    }
+}
